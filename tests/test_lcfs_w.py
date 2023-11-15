ec94a6c968f608df61aecae54a2ab848ed32d80c
Daniel Holmes <danielh839@gmail.com>
2023-11-15 14:13:33 -0500

    update lcfsw tests

--- tests/test_lcfs_w.py
+++ tests/test_lcfs_w.py
@@ -5,13 +5,12 @@ def test_lcfs_w():
     packets = [
         Packet(arrival_time=1, service_time=5, source=0),
         Packet(arrival_time=2, service_time=2, source=1),
-        Packet(arrival_time=3, service_time=2, source=0),
+        Packet(arrival_time=3, service_time=2, source=1),
     ]
 
     output = LCFS_W().simulate(packets)
 
     assert output == [
         PacketOutput(source=0, arrival_time=1, service_end_time=6),
-        PacketOutput(source=1, arrival_time=2, service_end_time=4),
-        PacketOutput(source=0, arrival_time=3, service_end_time=5),
+        PacketOutput(source=1, arrival_time=3, service_end_time=8),
     ]
