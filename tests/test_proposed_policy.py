fa3c32f62f6613c93104d26f4dbd94f3f29d09c7
Daniel Holmes <danielh839@gmail.com>
2023-11-15 14:19:15 -0500

    update proposed policy tests

--- tests/test_proposed_policy.py
+++ tests/test_proposed_policy.py
@@ -1,39 +1,18 @@
 from main import Packet, PacketOutput, ProposedPolicy
 
 
-def test_proposed_policy():
-    # Create a sequence of packets with whole number arrival times and service times
+def test_proposed():
     packets = [
-        Packet(arrival_time=0, service_time=1, source=1),  # Processed immediately
-        Packet(
-            arrival_time=2, service_time=6, source=2
-        ),  # Processed immediately because Packet 1 has already finished
-        Packet(arrival_time=3, service_time=7, source=2),  # Waiting in the Queue
-        Packet(
-            arrival_time=6, service_time=2, source=2
-        ),  # This one is of the same source as Packet 3, so it replaces it in the queue
+        Packet(arrival_time=1, service_time=5, source=0),
+        Packet(arrival_time=2, service_time=2, source=1),
+        Packet(arrival_time=3, service_time=2, source=0),
+        Packet(arrival_time=4, service_time=2, source=1),
     ]
 
-    # Run the simulation
-    policy = ProposedPolicy()
-    output = policy.simulate(packets)
+    output = ProposedPolicy().simulate(packets)
 
-    # Check the output
-    assert (
-        len(output) == 3
-    ), "Three packets should be processed."  # Because Packet 4 replaced Packet 3
-    assert (
-        output[0].source == 1 and output[0].service_end_time == 1
-    ), "First packet processed incorrectly."
-    assert (
-        output[1].source == 2 and output[1].service_end_time == 8
-    ), "Second packet processed incorrectly."
-    assert (
-        output[2].source == 2 and output[2].service_end_time == 10
-    ), "Third packet processed incorrectly."
-
-    print("Test passed!")
-
-
-# Run the test
-test_proposed_policy()
+    assert output == [
+        PacketOutput(arrival_time=1, service_end_time=6, source=0),
+        PacketOutput(arrival_time=4, service_end_time=8, source=1),
+        PacketOutput(arrival_time=3, service_end_time=10, source=0),
+    ]
