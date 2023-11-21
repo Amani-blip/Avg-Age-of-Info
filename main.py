45ef985710d99363f5eebd0e8cf683fd30b53745
Daniel Holmes <danielh839@gmail.com>
2023-11-21 13:36:18 -0500

    lcfs w incorrect version

--- main.py
+++ main.py
@@ -146,6 +146,88 @@ class LCFS_W:
         return output
 
 
+class LCFS_W_Incorrect:
+    def simulate(self, packets: list[Packet]) -> list[PacketOutput]:
+        # check there's at least one packet to simulate
+        if len(packets) == 0:
+            return []
+
+        # copy the packets since this method will modify the service_time
+        # as the packets are processed.
+        packets = [dataclasses.replace(packet) for packet in packets]
+
+        # run the simulation
+        last_update: list[float] = [-1, -1]
+        lcfs = Stack()
+
+        output: list[PacketOutput] = []
+
+        def process_packets(previous_clock: float, clock: float):
+            """given the previous clock time, and current clock time. we process
+            packets in the queue.
+            """
+            processing_clock = previous_clock
+
+            while not lcfs.empty() and processing_clock != clock:
+                packet: Packet = lcfs.pop()
+
+                # drop packets that contain old status updates
+                if packet.arrival_time < last_update[packet.source]:
+                    continue
+
+                # process the packet
+                available_processing_time = clock - processing_clock
+                processing_time = min(packet.service_time, available_processing_time)
+                processing_clock += processing_time
+                packet.service_time -= processing_time
+
+                # the packet was fully processed
+                if packet.service_time == 0:
+                    last_update[packet.source] = packet.arrival_time
+                    output.append(
+                        PacketOutput(
+                            source=packet.source,
+                            arrival_time=packet.arrival_time,
+                            service_end_time=processing_clock,
+                        )
+                    )
+                    continue
+
+                # the packet was not fully processed
+                # there wasn't enough processing time so we push it back to the top
+                # of the lcfs queue to continue processing the next time.
+                lcfs.push(packet)
+
+        previous_packet_arrival = -1
+
+        for packet in packets:
+            process_packets(previous_packet_arrival, packet.arrival_time)
+            previous_packet_arrival = packet.arrival_time
+
+            # if the stack is empty we put the new packet at the top
+            # otherwise we place the packet in the second position so
+            # that it waits before being processed
+            if lcfs.empty():
+                lcfs.push(packet)
+            else:
+                temp = lcfs.pop()
+                lcfs.push(packet)
+                lcfs.push(temp)
+
+        # process any remaining packets. packets with old status updates will be ignored.
+        # so I can calculate the total remaining processing time and advance the clock that far
+        # to ensure any remaining packets are processed.
+        total_remaining_processing_time = sum(
+            [packet.service_time for packet in lcfs.items]
+        )
+        process_packets(
+            previous_packet_arrival,
+            previous_packet_arrival + total_remaining_processing_time,
+        )
+
+        return output
+
+
 class LCFS_S:
     # Daniel
     def simulate(self, packets: list[Packet]) -> list[PacketOutput]:
@@ -226,7 +308,7 @@ class LCFS_S:
         return output
 
 
-class LCFS_S_Improved:
+class LCFS_S_Incorrect:
     # Daniel
     def simulate(self, packets: list[Packet]) -> list[PacketOutput]:
         # check there's at least one packet to simulate
